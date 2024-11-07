20ebceeadcd1d00477f5b84236b9532a695377d6
manankumar94 <mortal9193@gmail.com>
2024-11-07 15:20:24 +0530

    Route Actions

--- server/controllers/speedController.js
+++ server/controllers/speedController.js
@@ -1,18 +1,20 @@
-const { exec } = require('child_process');
+import { exec } from 'child_process';
 
-const getSpeed = (req, res) => {
-  exec('fast --json', (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).json({ error: 'Failed to run speed test' });
-    }
+class SpeedTestController {
+  static getSpeed(req, res) {
+    exec('fast --json', (error, stdout, stderr) => {
+      if (error) {
+        return res.status(500).json({ error: 'Failed to run speed test' });
+      }
 
-    try {
-      const result = JSON.parse(stdout);
-      res.json(result);
-    } catch (e) {
-      res.status(500).json({ error: 'Error parsing speed test data' });
-    }
-  });
-};
+      try {
+        const result = JSON.parse(stdout);
+        res.json(result);
+      } catch (e) {
+        res.status(500).json({ error: 'Error parsing speed test data' });
+      }
+    });
+  }
+}
 
-module.exports = { getSpeed };
+export default SpeedTestController;
