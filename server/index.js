efdfcef759ca5cbef7fec011a10ddfb7e2cf0fd1
manankumar94 <mortal9193@gmail.com>
2024-11-07 15:32:46 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const speedRoutes = require('./routes/speedRoutes');
+import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+
+import speedRoutes from "./routes/speedRoutes.js"
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.use(cors());
 app.use('/api/speed', speedRoutes);
